add4931fcd2158e70d8d0aa2ed7db2def5b11dfd
Shivam <shivamlather5@gmail.com>
2024-03-07 21:28:03 +0530

    Created common folder and uploaded to npm packages and also did type inference of the zod objects

---
+++ common/dist/index.d.ts
@@ -0,0 +1,51 @@
+import z from "zod";
+export declare const signupInput: z.ZodObject<{
+    name: z.ZodOptional<z.ZodString>;
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}, {
+    email: string;
+    password: string;
+    name?: string | undefined;
+}>;
+export declare const signinInput: z.ZodObject<{
+    email: z.ZodString;
+    password: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    email: string;
+    password: string;
+}, {
+    email: string;
+    password: string;
+}>;
+export declare const createBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+}, {
+    title: string;
+    content: string;
+}>;
+export declare const updateBlogInput: z.ZodObject<{
+    title: z.ZodString;
+    content: z.ZodString;
+    id: z.ZodString;
+}, "strip", z.ZodTypeAny, {
+    title: string;
+    content: string;
+    id: string;
+}, {
+    title: string;
+    content: string;
+    id: string;
+}>;
+export type SignupInput = z.infer<typeof signupInput>;
+export type SigninInput = z.infer<typeof signinInput>;
+export type CreateBlogInput = z.infer<typeof createBlogInput>;
+export type UpdateBlogInput = z.infer<typeof updateBlogInput>;
